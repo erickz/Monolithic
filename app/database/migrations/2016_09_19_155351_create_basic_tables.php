7d04def124a4aa88315a1c6e1038d36c5f880d53
Erick <erick@Mini-mac-Erick.local>
2016-09-19 13:07:48 -0300

    Migrations created

---
+++ app/database/migrations/2016_09_19_155351_create_basic_tables.php
@@ -0,0 +1,62 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBasicTables extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('users', function($table)
+	 {
+			 $table->increments('id');
+			 $table->string('email')->unique();
+			 $table->string('name');
+			 $table->timestamps();
+	 });
+
+		Schema::create('posts', function($table)
+		{
+			$table->increment();
+			$table->integer('user_id')->unsigned();
+			$table->string('name', 150);
+			$table->timestamps();
+		});
+
+		Schema::create('comments', function($table)
+		{
+			$table->increment();
+			$table->integer('post_id')->unsigned();
+			$table->text('text');
+			$table->timestamps();
+		});
+
+		Schema::table('posts', function($table)
+		{
+			$table->foreign('user_id')->references('id')->on('users');
+		});
+
+		Schema::table('comments', function($table)
+		{
+			$table->foreign('post_id')->references('id')->on('posts');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('posts');
+		Schema::drop('comments');
+		Schema::drop('users');
+	}
+
+}
