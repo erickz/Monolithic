2b051043c098e971d1a3c25b7073cbda340a3d17
Erick <erick@Mini-mac-Erick.local>
2016-12-02 13:06:33 -0200

    Removed the remember token from the migration of users

--- app/database/migrations/2016_09_19_155351_create_basic_tables.php
+++ app/database/migrations/2016_09_19_155351_create_basic_tables.php
@@ -18,7 +18,6 @@ class CreateBasicTables extends Migration {
 			$table->string('name');
 			$table->string('email')->unique();
 			$table->string('password');
-			$table->rememberToken();
 			$table->timestamps();
 	  });
 
